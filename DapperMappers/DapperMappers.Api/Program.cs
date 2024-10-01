0505a8ce0aa8303b32e22accd7725e1dd83f76b0
kubagdynia <kubagdynia@gmail.com>
2024-10-01 13:30:59 +0200

    Removed startup file and adapted code to current standards

--- DapperMappers/DapperMappers.Api/Program.cs
+++ DapperMappers/DapperMappers.Api/Program.cs
@@ -1,26 +1,59 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using System.Text.Json.Serialization;
+using DapperMappers.Domain.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using DapperMappers.Api.Extensions;
+using DapperMappers.Api.Serializers;
+using DapperMappers.Domain.Extensions;
+using DapperMappers.Domain.Repositories.CommandQueries;
+using DbConnectionExtensions.DbConnection;
 
-namespace DapperMappers.Api
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddAutoMapper(typeof(Program))
+    .AddSingleton<ICommandQuery, CommandQuery>()
+    
+    //.AddTransient<IDbConnectionFactory, DbConnectionFactory>();
+    .AddTransient<IDbConnectionFactory>(x =>
+        ActivatorUtilities.CreateInstance<DbConnectionFactory>(x, "DefaultConnection"))
+    
+    .AddScoped<IBookRepository, BookRepository>()
+    //.AddScoped<IBookRepository>(x =>
+    //     ActivatorUtilities.CreateInstance<BookRepository>(x, "DefaultConnection"));
+    
+    .AddDomain()
+    
+    .AddSwagger<Program>(includeXmlComments: true, name: "v1", title: "Book API", version: "v1")
+    
+    .AddControllers().AddJsonOptions(options =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        // The default value is Include, which includes properties with null values in the serialized output.
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        // The default value is false, which means that the case of property names is preserved.
+        options.JsonSerializerOptions.PropertyNamingPolicy = BaseJsonOptions. PropertyNamingPolicy;
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+app.UseRouting(); 
+
+app.UseAuthorization();
+
+app.UseCustomSwagger("/swagger/v1/swagger.json", "Book API V1");
+
+app.MapControllers();
+
+app.Run();
+
+
+    
+    
